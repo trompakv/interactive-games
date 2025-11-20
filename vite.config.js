852a64e0896d470f5810be0aea3a1618bd2f402d
trompakv <trompakv2@gmail.com>
2025-11-20 18:49:41 +0200

    settings

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,19 @@
-import { defineConfig } from 'vite';
-import glob from 'glob';
-import injectHTML from 'vite-plugin-html-inject';
-import FullReload from 'vite-plugin-full-reload';
+import { defineConfig } from "vite";
+import glob from "glob";
+import injectHTML from "vite-plugin-html-inject";
+import FullReload from "vite-plugin-full-reload";
 
 export default defineConfig({
   define: {
     global: {},
   },
-  root: 'src',
+  root: "src",
   build: {
     rollupOptions: {
-      input: glob.sync('./src/*.html'),
+      input: glob.sync("./src/*.html"),
     },
-    outDir: '../dist',
+    outDir: "../dist",
   },
-  plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
-  base: '/interactive-games/',
+  plugins: [injectHTML(), FullReload(["./src/**/**.html"])],
+  //   base: "/interactive-games/",
 });
