04bd16d48c2548f6ee69655601c23ca5d180a37c
trompakv <trompakv2@gmail.com>
2025-12-25 17:54:32 +0200

    fix

--- src/js/modalWindow.js
+++ src/js/modalWindow.js
@@ -2,6 +2,8 @@ window.addEventListener("load", () => {
     const modal = document.getElementById("modal");
     const modalContent = modal.querySelector(".modal-content");
     const closeBtn = modal.querySelector(".close");
+    const input = modal.querySelector(".name-input");
+    const saveBtn = modal.querySelector(".save-btn");
 
     setTimeout(() => {
         modal.style.display = "flex";
@@ -13,6 +15,11 @@ window.addEventListener("load", () => {
 
     modal.addEventListener("click", () => {
         modal.style.display = "none";
+    });
+
+    saveBtn.addEventListener("click", () => {
+        modal.style.display = "none";
+        document.getElementById("username").textContent = input.value;
     });
 
     modalContent.addEventListener("click", (e) => {
